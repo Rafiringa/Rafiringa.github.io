3be60596bd6e8ab2e85e6e2ce8dcac61881db722
Ampy <rafiringa.a@gmail.com>
2023-09-12 21:42:43 +0300

    Ajout effet popup image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
+  // Navbar apparence
   if (window.scrollY == 0) {
     navbar.style.background = "none";
   } else if (window.scrollY < lastScroll) {
@@ -10,4 +11,18 @@ window.addEventListener("scroll", () => {
     navbar.style.top = "-60px";
   }
   lastScroll = window.scrollY;
+
+  // Image apparence
+  let scrollValue =
+    (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+
+  if (scrollValue > 0.3) {
+    imgImprovise1.style.opacity = 1;
+    imgImprovise1.style.transform = "none";
+  }
+
+  if (scrollValue > 0.35) {
+    imgImprovise2.style.opacity = 1;
+    imgImprovise2.style.transform = "none";
+  }
 });
